874b40ced0db1db645fdb4820e7dfd2dbf682549
Alan Mels <me@alanmels.com>
2022-06-23 22:45:48 -0500

    Resolves https://github.com/backdrop-contrib/invite/issues/7

--- js/invite.js
+++ js/invite.js
@@ -6,7 +6,7 @@
 
 Backdrop.behaviors.invite_user = {
   attach: function (context) {
-    $('#invite-user-overview .invite-reg-link').click(function(e) {
+    $('.invite-reg-link').click(function(e) {
       $('#invite-reg-link-container #invite-reg-title').html($(this).attr('title') + ':');
       $('#invite-reg-link-container #invite-reg-link').html($(this).attr('href'));
       $('#invite-reg-link-container').fadeIn();
